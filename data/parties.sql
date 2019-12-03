83a05c8c2d2bda4f7f9fc548f2fe9542556e8405
Julien Capellari <julien@capellari.net>
2019-12-03 20:16:43 +0100

    remove data

--- data/parties.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO le_voyage_dans_l_espace_partie (id, numero, titre, slug) VALUES (1, 0, 'Début', 'debut');
-INSERT INTO le_voyage_dans_l_espace_partie (id, numero, titre, slug) VALUES (2, 1, 'Aller dans l''espace', 'aller-dans-lespace');
-INSERT INTO le_voyage_dans_l_espace_partie (id, numero, titre, slug) VALUES (3, 2, 'Les problèmes posés', 'les-problemes-poses');
-INSERT INTO le_voyage_dans_l_espace_partie (id, numero, titre, slug) VALUES (4, 3, 'Aller aussi vite que la lumière', 'aller-aussi-vite-que-la-lumiere');
-INSERT INTO le_voyage_dans_l_espace_partie (id, numero, titre, slug) VALUES (5, 4, 'fin', 'fin');
